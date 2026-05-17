7aea2b58c73f74856d935814eebdad20ab3275f3
sooho-shin <soojoon9200@gmail.com>
2026-05-17 18:15:20 +0900

    Show winner banner on result page

--- src/components/templates/ResultTemplate.tsx
+++ src/components/templates/ResultTemplate.tsx
@@ -1,4 +1,5 @@
 import { GameHeader } from "@/components/organisms/GameHeader";
+import { DailyWinnerBanner } from "@/components/organisms/DailyWinnerBanner";
 import { ResultPanel } from "@/components/organisms/ResultPanel";
 
 export function ResultTemplate() {
@@ -6,6 +7,7 @@ export function ResultTemplate() {
     <main className="app-shell">
       <div className="screen-frame">
         <GameHeader eyebrow="Result" title="결과" />
+        <DailyWinnerBanner />
         <ResultPanel />
       </div>
     </main>
